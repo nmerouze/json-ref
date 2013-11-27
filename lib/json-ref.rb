25b568729e4e2f264bfb3c546d90a2411f1db786
Nicolas Mérouze <nicolas.merouze@gmail.com>
2013-11-27 21:04:30 +0100

    Add file path support for $ref

--- lib/json-ref.rb
+++ lib/json-ref.rb
@@ -1,6 +1,7 @@
-# encoding: utf-8
+require "json"
+
 class JSONRef
-  VERSION = '0.1.0'
+  VERSION = "0.2.0"
   
   def initialize(document)
     @document = document
@@ -8,8 +9,12 @@ class JSONRef
 
   def expand
     @document.each do |k,v|
-      if v.is_a?(Hash) && v.has_key?('$ref')
-        @document[k] = split_path(v['$ref']).inject(@document) { |doc, item| doc[item] }
+      if v.is_a?(Hash) && v.has_key?("$ref")
+        if v["$ref"] =~ /\.json$/
+          @document[k] = read_file(v["$ref"])
+        else
+          @document[k] = split_path(v["$ref"]).inject(@document) { |doc, item| doc[item] }
+        end
       end
     end
   end
@@ -21,11 +26,15 @@ class JSONRef
   private
 
   def split_path(path)
-    escape_characters = {'^/' => '/', '^^' => '^', '~0' => '~', '~1' => '/'}
-    return [''] if path == '#/'
+    escape_characters = {"^/" => "/", "^^" => "^", "~0" => "~", "~1" => "/"}
+    return [""] if path == "#/"
 
-    path.sub(/^#\//, '').split(/(?<!\^)\//).map! { |part|
+    path.sub(/^#\//, "").split(/(?<!\^)\//).map! { |part|
       part.gsub!(/\^[\/^]|~[01]/) { |m| escape_characters[m] }; part
     }
+  end
+
+  def read_file(path)
+    JSON.load(File.read(path))
   end
 end
--- test/test_json-ref.rb
+++ test/test_json-ref.rb
@@ -1,23 +1,30 @@
-require 'helper'
+require "helper"
 
 class TestJSONRef < MiniTest::Unit::TestCase
   def test_expand_string
-    origin_doc = { 'title' => 'foobar', 'ref_title' => { '$ref' => '#/title' } }
-    result_doc = { 'title' => 'foobar', 'ref_title' => 'foobar' }
+    origin_doc = { "title" => "foobar", "ref_title" => { "$ref" => "#/title" } }
+    result_doc = { "title" => "foobar", "ref_title" => "foobar" }
 
     assert_equal result_doc, JSONRef.new(origin_doc).expand
   end
 
   def test_expand_hash
-    origin_doc = { 'title' => { 'value' => 'foobar' }, 'ref_title' => { '$ref' => '#/title' } }
-    result_doc = { 'title' => { 'value' => 'foobar' }, 'ref_title' => { 'value' => 'foobar' } }
+    origin_doc = { "title" => { "value" => "foobar" }, "ref_title" => { "$ref" => "#/title" } }
+    result_doc = { "title" => { "value" => "foobar" }, "ref_title" => { "value" => "foobar" } }
 
     assert_equal result_doc, JSONRef.new(origin_doc).expand
   end
 
-  def test_expand_complex_path
-    origin_doc = { 'nested' => { 'title' => 'foobar' }, 'ref_title' => { '$ref' => '#/nested/title' } }
-    result_doc = { 'nested' => { 'title' => 'foobar' }, 'ref_title' => 'foobar' }
+  def test_expand_nested_path
+    origin_doc = { "nested" => { "title" => "foobar" }, "ref_title" => { "$ref" => "#/nested/title" } }
+    result_doc = { "nested" => { "title" => "foobar" }, "ref_title" => "foobar" }
+
+    assert_equal result_doc, JSONRef.new(origin_doc).expand
+  end
+
+  def test_file_path
+    origin_doc = { "file" => { "$ref" => "test/fixtures/file.json" } }
+    result_doc = { "file" => { "title" => "My PDF", "path" => "/path/to/my.pdf" } }
 
     assert_equal result_doc, JSONRef.new(origin_doc).expand
   end
